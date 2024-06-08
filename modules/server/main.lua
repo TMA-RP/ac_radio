f77995f53444a12952c59804547b259df6011673
Cyril Beeckman <cyril.beeckman@gmail.com>
2024-06-08 21:01:05 +0200

    fix convar

--- modules/server/main.lua
+++ modules/server/main.lua
@@ -6,11 +6,6 @@ lib.versionCheck('acscripts/ac_radio')
 SetConvarReplicated('radio:disconnectWithoutRadio', tostring(Config.disconnectWithoutRadio))
 SetConvarReplicated('radio_noRadioDisconnect', tostring(Config.disconnectWithoutRadio)) -- backwards compatibility
 
-SetConvarReplicated('voice_useNativeAudio', tostring(Config.radioEffect))
-SetConvarReplicated('voice_enableSubmix', Config.radioEffect and '1' or '0')
-SetConvarReplicated('voice_enableRadioAnim', Config.radioAnimation and '1' or '0')
-SetConvarReplicated('voice_defaultRadio', Config.radioTalkKey)
-
 SetTimeout(0, function()
     if Utils.hasExport('ox_core.GetPlayer') then
         require 'modules.server.framework.ox'
